3abbef214b45bb0ddad26e64233bf8345bb7e9b0
Richel Sensineli <richelsensineli@gmail.com>
2019-04-13 21:49:46 -0300

    Obtaining data from inputs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { CredenciaisDTO } from './../../models/credenciaisDTO';
 import { Component } from '@angular/core';
 import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
@@ -8,18 +9,24 @@ import { NavController, IonicPage, MenuController } from 'ionic-angular';
 })
 export class HomePage {
 
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
+  };
+
   constructor(public navCtrl: NavController, public menu: MenuController) {
 
   }
 
   login(){
+    console.log(this.creds);
     this.navCtrl.setRoot('CategoriasPage')
   }
 
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
   }
-  
+
   ionViewDidLeave() {
     this.menu.swipeEnable(true);
   }
